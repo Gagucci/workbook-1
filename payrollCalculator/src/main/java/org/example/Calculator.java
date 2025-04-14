5eb59d416bd098726bc5e4aa3692ec9cb2022ece
Kevin Gagante <KevinGagante@Gmail.com>
2025-04-14 11:24:07 -0700

    Refactored Exercise 4
    Refactored code to be more readable

--- payrollCalculator/src/main/java/org/example/Calculator.java
+++ payrollCalculator/src/main/java/org/example/Calculator.java
@@ -2,23 +2,27 @@ package org.example;
 import java.util.Scanner;
 
 public class Calculator {
+        static Scanner read = new Scanner(System.in);
+        static String name;
+        static float hours;
+        static float payRate;
+        static float grossPay;
     public static void promptCalculation() {
-        String name;
-        float hours;
-        float payRate;
-        float grossPay;
 
-        Scanner read = new Scanner(System.in);
         System.out.println("Welcome to the Pay Calculator App!");
         System.out.println("Please enter your Name");
         name = read.nextLine();
 
-        System.out.println("Please enter your hours worked");
+        System.out.println("Please enter your hours worked these past 2 weeks");
         hours = read.nextFloat();
 
-        System.out.println("Please enter your pay rate");
+        System.out.println("Please enter your hourly pay rate");
         payRate = read.nextFloat();
 
+
+    }
+
+    public static void grossPay() {
         grossPay = hours * payRate;
         System.out.println("Hello " + name + ", your gross pay is: " + grossPay);
 
--- payrollCalculator/src/main/java/org/example/Main.java
+++ payrollCalculator/src/main/java/org/example/Main.java
@@ -1,11 +1,10 @@
 package org.example;
 
-
 public class Main {
     public static void main(String[] args) {
 
         Calculator.promptCalculation();
-
+        Calculator.grossPay();
 
     }
 }
