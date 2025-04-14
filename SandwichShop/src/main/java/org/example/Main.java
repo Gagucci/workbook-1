8bcd690105b9eb5b184f6f0ad323523426212a7e
Kevin Gagante <KevinGagante@Gmail.com>
2025-04-14 15:03:40 -0700

    Added Loaded option for the sandwich shop

--- SandwichShop/src/main/java/org/example/Main.java
+++ SandwichShop/src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
         SandwichTransaction.greetCustomer();
         SandwichTransaction.promptOrder();
+        SandwichTransaction.loadedCalculator();
         SandwichTransaction.calculateTotal();
 
     }
--- SandwichShop/src/main/java/org/example/SandwichTransaction.java
+++ SandwichShop/src/main/java/org/example/SandwichTransaction.java
@@ -8,6 +8,7 @@ public class SandwichTransaction {
     static String foodName;
     static String reg = "Regular Joe";
     static String lrg = "Large Joe";
+    static String isLoaded;
 
     static int age;
     static double regSandwich = 5.45;
@@ -16,19 +17,18 @@ public class SandwichTransaction {
     static double total;
 
     public static void greetCustomer() {
-
         System.out.println("Welcome to Mo's Sloppy Joe's!\nWhat's your name fella?");
         name = read.nextLine();
         System.out.println("And how old are you, " + name);
         age = read.nextInt();
-
+        read.nextLine();
     }
 
     public static void promptOrder() {
-
         System.out.println("Glad to have you " + name + "!\nWhat will your order be? Please enter a number.");
         System.out.println("Menu:\n --Sandwiches--\n1- Regular Joe\n2 - Large Joe");
         order = read.nextDouble();
+        read.nextLine();
         if (order == 1) {
             order = regSandwich;
             foodName = reg;
@@ -40,8 +40,19 @@ public class SandwichTransaction {
         }
     }
 
-    public static void calculateTotal() {
+    public static void loadedCalculator() {
+        System.out.println("Would you like your " + foodName + " loaded?\nChoices:\nYes / No");
+        isLoaded = read.nextLine();
+        if (isLoaded.equals("Yes") && order == regSandwich){
+            order = order + 1.0;
+        } else if (isLoaded.equals("Yes") && order == lrgSandwich) {
+            order = order + 1.75;
+        } else if (isLoaded.equals("No")) {
+            System.out.println("No worries!");
+        }
+    }
 
+    public static void calculateTotal() {
         if (age <= 17) {
             total = ( order + (order * .10) );
             System.out.println("You get a 10% discount kiddo!");
@@ -53,10 +64,8 @@ public class SandwichTransaction {
         }
 
         String formattedTotal = String.format("%.2f", total);
-
-        System.out.println(
-                "Alrighty " + name + ",\nyou ordered a " + foodName + "\nyour total will be " + formattedTotal );
-
+        System.out.println("Alrighty " + name + ",\nyou ordered a " + foodName + "\nyour total will be " + formattedTotal );
+        System.out.println("Have a great day!");
     }
 
 }
